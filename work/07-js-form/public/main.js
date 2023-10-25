b321fef4f2b202bf1ae6261f1daaa615ff4c4583
ZhengruiLu <lu.zhengr@northeastern.edu>
2023-10-24 21:14:05 -0700

    add elements

--- work/07-js-form/public/main.js
+++ work/07-js-form/public/main.js
@@ -4,21 +4,23 @@
   console.log(formEl);
   const emailGroupEl = document.querySelector('.register__email');
   const confirmGroupEl = document.querySelector('.register__confirm');
-  const emailInputEl = document.querySelector('register-email__input');
-  const confirmInputEl = document.querySelector('register-confirm__input');
+  const emailInputEl = document.querySelector('.register-email__input');
+  const confirmInputEl = document.querySelector('.register-confirm__input');
 
   formEl.addEventListener('submit', (event) => {
     let isInvalid = false;
-    emailGroupEl.classList.remove('register-email--invalid');
-    confirmGroupEl.classList.remove('register-confirm--invalid');
+    emailGroupEl.classList.remove('.register-email__error');
+    // emailGroupEl.classList.remove('.register-email--invalid');
+    confirmGroupEl.classList.remove('.register-confirm--invalid');
 
     if( !emailInputEl.value ) {
-      emailGroupEl.classList.add('register-email--invalid');
+      emailGroupEl.classList.add('.register-email__error');
+      // emailGroupEl.classList.add('.register-email--invalid');
       isInvalid = true;
     }
 
     if( !confirmInputEl.value ) {
-      confirmGroupEl.classList.add('register-confirm--invalid');
+      confirmGroupEl.classList.add('.register-confirm--invalid');
       isInvalid = true;
     }
 
