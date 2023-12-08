184b019267394292519ba9bf540a9e092af70ee8
ZhengruiLu <lu.zhengr@northeastern.edu>
2023-12-08 09:35:32 -0800

    move close btn to the top right corner of the modal

--- final/cat-app/src/Modal.jsx
+++ final/cat-app/src/Modal.jsx
@@ -12,11 +12,19 @@ function Modal({
     setUsername(name)
   }
     return (
-      <dialog className="modal" ref={modalRef} >
+      <dialog className="modal" ref={modalRef}>
+        <button className="close-button" onClick={() => modalRef.current.close()}>
+          Close
+        </button>
         <h2>{username && <p>Hello {username}</p>}</h2>
         <RegisterForm onRegister={onRegister}></RegisterForm>
-        <button onClick={() => modalRef.current.close()}>Close</button>
       </dialog>
+
+      // <dialog className="modal" ref={modalRef} >
+      //   <button onClick={() => modalRef.current.close()}>Close</button>
+      //   <h2>{username && <p>Hello {username}</p>}</h2>
+      //   <RegisterForm onRegister={onRegister}></RegisterForm>
+      // </dialog>
     )
 }
 
